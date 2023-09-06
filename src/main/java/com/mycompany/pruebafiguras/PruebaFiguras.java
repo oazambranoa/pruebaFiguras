3fa8f014a9d4e1255b0cb967e0b0deaf1fce8f7d
oazambranoa <oazambranoa@unal.edu.co>
2023-09-06 12:28:05 -0500

    Inicializacion y crecio de las primeras clases

---
+++ src/main/java/com/mycompany/pruebafiguras/PruebaFiguras.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pruebafiguras;
+
+/**
+ *
+ * @author omarz
+ */
+public class PruebaFiguras {
+
+    public static void main(String[] args) {
+        
+        Circulo figura1 = new Circulo(2);
+    }
+}
+    
